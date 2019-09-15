b19392b16215da773d5681eb5c3efb78111f3364
ayjayy7 <eng.amonijoni@gmail.com>
2019-09-15 12:51:18 +0300

    theres an error

--- functions.js
+++ functions.js
@@ -6,6 +6,13 @@
  */
 function greet(name) {
   // Your code here
+  if (name){
+    console.log(`Hello ${name}`);
+  }
+  else {
+    console.log("Hello");
+  }
+
 }
 
 /**
@@ -15,6 +22,12 @@ function greet(name) {
  */
 function isOdd(n) {
   // Your code here
+  if (n % 2 !==0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 /**
@@ -30,6 +43,7 @@ function isOdd(n) {
  */
 function oddsSmallerThan(n) {
   // Your code here
+  return (n - n%2)/2
 }
 
 /**
@@ -44,6 +58,12 @@ function oddsSmallerThan(n) {
  */
 function squareOrDouble(n) {
   // Your code here
+  if (isOdd(n)) {
+    return n*n;
+  }
+  else {
+    return n*2;
+  }
 }
 
 /**
@@ -64,9 +84,18 @@ function squareOrDouble(n) {
  */
 function ageFromBirthDate(birthDate) {
   const _MS_PER_YEAR = 1000 * 60 * 60 * 24 * 365;
-
   // Your code here
-}
+  
+  const year = birthDate.slice(0,4);
+  const month = birthDate.slice(4,6);
+  const day = birthDate.slice(6); // or (6,8)
+  const today = new Date();
+  const dob = new Date('${month}/${day}/${year}');
+  let age = today - dob;
+  age = age / _MS_PER_YEAR;
+  return Math.floor(age); //get int not float
+ }
+ 
 
 module.exports = {
   greet,
